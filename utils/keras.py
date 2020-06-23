6af06e76f3e92e7f8cb37ddffe3622b6935a30db
jason <anyway2367@gmail.com>
2020-06-24 02:47:09 +0800

    add focal loss

--- utils/keras.py
+++ utils/keras.py
@@ -1,5 +1,6 @@
-# import tensorflow.compat.v1 as tf
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+from tensorflow.keras import backend as K
+# import tensorflow as tf
 import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +9,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from sklearn.metrics import confusion_matrix
-
+from datetime import datetime
 
 class CnnModel:
     index = 0
@@ -24,30 +25,29 @@ class CnnModel:
     # dense_config:
     # {'unit': 128}
     dense_config_list = list()
-    model = Sequential()
+    model = None
     drop_out_rate = 0.25
     validation_split = 0.2
+    current_time = datetime.now()
 
-    def __init__(self, index, x_train, y_train, x_test, y_test, batch_size, epochs, conv2D_config_list, dense_config_list):
-        self.index = index
+    def __init__(self, x_train, y_train, x_test, y_test, loss="categorical_crossentropy"):
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
         self.y_test = y_test
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.conv2D_config_list = conv2D_config_list
-        self.dense_config_list = dense_config_list
+        self.loss = loss
 
     def _compile_model(self):
+        self.model = Sequential()
+
         for config in self.conv2D_config_list:
             # 建立卷積層，filter=16,即 output space 的深度, Kernal Size: 5x5, activation function 採用 relu
             self.model.add(Conv2D(filters=config.get('filters'),
                                   kernel_size=config.get('kernel_size'),
                                   padding=config.get('padding', 'same'),
                                   activation=config.get('activation', 'relu'),
-                                  input_shape=config.get('input_shape')),
-                           strides=config.get('strides', (2, 2)))
+                                  input_shape=config.get('input_shape'),
+                                  strides=config.get('strides')))
             # 建立池化層，池化大小=2x2，取最大值
             self.model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -65,10 +65,10 @@ class CnnModel:
         self.model.add(Dropout(self.drop_out_rate))
 
         # 使用 softmax activation function，將結果分類
-        self.model.add(Dense(self.y_train[1], activation='softmax'))
+        self.model.add(Dense(np.shape(self.y_test[1])[0], activation='softmax'))
 
         # 編譯: 選擇損失函數、優化方法及成效衡量方式
-        self.model.compile(loss="categorical_crossentropy",
+        self.model.compile(loss=self.loss,
                            optimizer="adam",
                            metrics=['accuracy'])
 
@@ -76,28 +76,33 @@ class CnnModel:
         print(self.model.summary())
 
     def _output_history(self, train_history, test_loss, test_accuracy, sensitivity, specificity):
-
-        accuracy = train_history.history['accuracy']
-        val_accuracy = train_history.history['val_accuracy']
-        loss = train_history.history['loss']
-        val_loss = train_history.history['val_loss']
-        with open("model_result.txt", "a") as my_file:
-            my_file.write(f'{self.index+1},{accuracy},{val_accuracy},{loss},{val_loss},{self.test_loss},{self.test_accuracy},{self.sensitivity},{self.specificity}\n')
-        pass
+        filename = f"model_result_{datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+        accuracy = train_history.history['accuracy'][-1]
+        val_accuracy = train_history.history['val_accuracy'][-1]
+        loss = train_history.history['loss'][-1]
+        val_loss = train_history.history['val_loss'][-1]
+        with open(filename, "a") as my_file:
+            my_file.write(f'{self.index+1},{accuracy},{val_accuracy},{loss},{val_loss},{test_loss},{test_accuracy},{sensitivity},{specificity}\n')
+        print(f"===file output to {filename}===")
 
     def _get_sensitivity_specificity(self, predictions, y_test):
         y_test = np.argmax(y_test, axis=-1)
         predictions = np.argmax(predictions, axis=-1)
 
         c = confusion_matrix(y_test, predictions)
-        print('Confusion matrix:')
-        print(c)
-        print('sensitivity = ', c[0, 0] / (c[0, 1] + c[0, 0]))
-        print('specificity = ', c[1, 1] / (c[1, 1] + c[1, 0]))
+        # print('Confusion matrix:')
+        # print(c)
+        # print('sensitivity = ', c[0, 0] / (c[0, 1] + c[0, 0]))
+        # print('specificity = ', c[1, 1] / (c[1, 1] + c[1, 0]))
 
         return c[0, 0] / (c[0, 1] + c[0, 0]), c[1, 1] / (c[1, 1] + c[1, 0])
 
-    def run(self):
+    def run(self, index, batch_size, epochs, conv2D_config_list, dense_config_list, enable_log=0):
+        self.index = index
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.conv2D_config_list = conv2D_config_list
+        self.dense_config_list = dense_config_list
         # 編譯 model
         self._compile_model()
         # 進行訓練, 訓練過程會存在 train_history 變數中
@@ -106,7 +111,7 @@ class CnnModel:
                                        batch_size=self.batch_size,
                                        epochs=self.epochs,
                                        validation_split=self.validation_split,
-                                       verbose=1)
+                                       verbose=enable_log)
 
         # 顯示損失函數、訓練成果(分數)
         score = self.model.evaluate(self.x_test, self.y_test, verbose=0)
@@ -114,28 +119,36 @@ class CnnModel:
         sensitivity, specificity = self._get_sensitivity_specificity(predictions, self.y_test)
         test_loss = score[0]
         test_accuracy = score[1]
-        print('Test loss:', test_loss)
+        last_accuracy = train_history.history['accuracy'][-1]
+        last_loss = train_history.history['loss'][-1]
+        print('Train accuracy:', last_accuracy)
+        print('Train loss:', last_loss)
         print('Test accuracy:', test_accuracy)
+        print('Test loss:', test_loss)
         print('Sensitivity:', sensitivity)
         print('Specificity:', specificity)
-        self._output_history(self, train_history, test_loss, test_accuracy, sensitivity, specificity)
+        self._output_history(train_history, test_loss, test_accuracy, sensitivity, specificity)
+
+        K.clear_session()
+        return train_history
 
 
-def show_train_history(train_history, t, v):
-    plt.plot(train_history.history['accuracy'])
-    plt.plot(train_history.history['val_accuracy'])
-    plt.title('Train History')
-    plt.xlabel('Epoch')
-    plt.ylabel('accuracy')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
-
-    plt.plot(train_history.history['loss'])
-    plt.plot(train_history.history['val_loss'])
-    plt.title('Train History')
-    plt.xlabel('Epoch')
-    plt.ylabel('loss')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.show()
+def show_train_history(train_history, type='accuracy'):
+    if type == 'accuracy':
+        plt.plot(train_history.history['accuracy'])
+        plt.plot(train_history.history['val_accuracy'])
+        plt.title('Train History')
+        plt.xlabel('Epoch')
+        plt.ylabel('accuracy')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+    else:
+        plt.plot(train_history.history['loss'])
+        plt.plot(train_history.history['val_loss'])
+        plt.title('Train History')
+        plt.xlabel('Epoch')
+        plt.ylabel('loss')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
 
 
